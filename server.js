7029e0662404df49ffbba8ce2e119eeb6e3536fe
KeXuan636 <kexuanlee0709@gmail.com>
2026-01-08 14:27:38 +0800

    hi

--- server.js
+++ server.js
@@ -43,3 +43,22 @@ app.post('/addmovie', async (req, res) => {
         res.status(500).json({ message: 'Server error - could not add movie '+title });
     }
 });
+
+app.put("/movies/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, genre, rating } = req.body;
+
+    const result = await pool.query(
+        "UPDATE movies SET title=$1, genre=$2, rating=$3 WHERE id=$4 RETURNING *",
+        [title, genre, rating, id]
+    );
+
+    res.json(result.rows[0]);
+});
+
+app.delete("/movies/:id", async (req, res) => {
+    const { id } = req.params;
+
+    await pool.query("DELETE FROM movies WHERE id=$1", [id]);
+    res.json({ message: "Movie deleted" });
+});
